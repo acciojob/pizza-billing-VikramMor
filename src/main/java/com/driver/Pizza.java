6ef2e3e6675039a9499fa9e35439ac8ccec5edfd
VikramMor <er.vickymor@gmail.com>
2022-12-06 15:33:58 +0530

    all functions and parameters added

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int extraCheese;
+    private int extraTopping;
+    private int paperBag;
+    private int totalPrice;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg){
+            totalPrice += 300;
+            this.price = 300;
+        }
+        else{
+            totalPrice += 400;
+            this.price = 400;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +27,52 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(isVeg){
+            if(this.price == 300){
+                this.extraCheese = 80;
+                this.totalPrice += 80;
+            }
+        }
+        if(!isVeg){
+            if(this.price == 400){
+                this.extraCheese = 80;
+                this.totalPrice += 80;
+            }
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(isVeg){
+            if(this.price == 300){
+                this.extraCheese = 70;
+                this.totalPrice += 70;
+            }
+        }
+        if(!isVeg){
+            if(this.price == 400){
+                this.extraCheese = 120;
+                this.totalPrice += 120;
+            }
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        this.paperBag = 20;
+        this.totalPrice += 20;
     }
 
     public String getBill(){
-        // your code goes here
+        this.bill += "Base Price Of The Pizza: " + this.price;
+        if(extraCheese>0){
+            this.bill += "\nExtra Cheese Added: "+this.extraCheese;
+        }
+        if(extraTopping>0){
+            this.bill += "\nExtra Toppings Added: "+this.extraTopping;
+        }
+        if(paperBag>0){
+            this.bill += "\nPaperbag Added: "+this.paperBag;
+        }
+        this.bill += "\nTotal Price: "  + this.totalPrice;
         return this.bill;
     }
 }
